ebfb0a4eae315781d8e5efa50403d570246c84fb
brunopeople <brunopessoa.melo@gmail.com>
2019-10-11 21:45:12 -0300

    criação do componente dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
     CommonModule
+  ],
+
+  exports: [
+    DashboardComponent
   ]
 })
 export class DashboardModule { }
--- src/app/dashboard/index.ts
+++ src/app/dashboard/index.ts
@@ -1,1 +1,2 @@
-export * from './dashboard.module'
+export * from './dashboard.module';
+export * from './dashboard.component';
