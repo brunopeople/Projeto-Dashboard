7f210928158b8c8991e7fd68d60ff7a01603ca35
Marcio <m4rcio.souza@gmail.com>
2019-10-17 10:22:03 +0100

    Fix para a exibição dos gráficos.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { DadosService } from './dados.service';
 import { Observable } from 'rxjs/Observable';
+
+import { DadosService } from './dados.service';
 
 declare var google: any;
 
@@ -13,117 +14,130 @@ export class DashboardComponent implements OnInit {
 
   private dados: any;
 
-  constructor(private dadosService: DadosService) { }
+  constructor(private dadosService: DadosService) {}
 
   ngOnInit() {
-    this.dadosService.obterDados().subscribe(
-      dados => {
-        this.dados = dados;
-        this.init();
-      });
+  	this.dadosService.obterDados().subscribe(
+  		dados => {
+  			this.dados = dados;
+  			this.init();
+  		});
   }
-  
+
   /**
-   * A função init() incializa a API de gráficos com delay de 1 segundo, o que permite
-   * a integração da API com o Angular.
+   * Inicializa a API de gráficos com delay de 1 segundo,
+   * o que permite a integração da API com o Angular.
+   *
    * @return void
    */
   init(): void {
-    if (typeof (google) !== 'undefined') {
-      google.charts.load('current', { 'packages': ['corechart'] });
+    if(typeof(google) !== 'undefined') {
+      google.charts.load('current', {'packages':['corechart']});
       setTimeout(() => {
-        google.charts.setOnloadCallback(this.exibirGraficos());
+      	google.charts.setOnLoadCallback(this.exibirGraficos());
       }, 1000);
     }
   }
 
   /**
    * Método chamado assim que a API de gráficos é inicializada.
-   * Responsável por chamar os métodos geradores dos gráficos.
-   *
+   * Reponsável por chamar os métodos geradores dos gráficos.
    *
    * @return void
    */
-
   exibirGraficos(): void {
-    this.exibirPieChart();
-    this.exibir3dPieChart();
-    this.exibirBarChart();
-    this.exibirLineChart();
-    this.exibirColumnChart();
-    this.exibirDonutChart();
+  	this.exibirPieChart();
+  	this.exibir3dPieChart();
+  	this.exibirBarChart();
+  	this.exibirLineChart();
+  	this.exibirColumnChart();
+  	this.exibirDonutChart();
   }
 
-  /**Exibe o gráfico Pie Chart.
+  /**
+   * Exibe o gráfico Pie Chart.
    *
    * @return void
-   **/
-
+   */
   exibirPieChart(): void {
-    const el = document.getElementById('pie_chart');
+  	const el = document.getElementById('pie_chart');
     const chart = new google.visualization.PieChart(el);
 
     chart.draw(this.obterDataTable(), this.obterOpcoes());
   }
 
   /**
-   *Exibe o gráfico Pie Chart em 3D.
+   * Exibe o gráfico Pie Chart em 3D. 
    *
    * @return void
    */
-
   exibir3dPieChart(): void {
-    const el = document.getElementById('3d_pie_chart');
-    const chart = new google.visualization.PieChart(el);
-    const opcoes = this.obterOpcoes();
+  	const el = document.getElementById('3d_pie_chart');
+  	const chart = new google.visualization.PieChart(el);
+	const opcoes = this.obterOpcoes();
 
     opcoes['is3D'] = true;
     chart.draw(this.obterDataTable(), opcoes);
   }
 
   /**
-   *Exibe o gráfico Donut Chart
-   *@return void
-   * */
-
+   * Exibe o gráfico Donut Chart.
+   *
+   * @return void
+   */
   exibirDonutChart(): void {
-    const el = document.getElementById('donut_chart');
-    const chart = new google.visualization.PieChart(el);
+  	const el = document.getElementById('donut_chart');
+  	const chart = new google.visualization.PieChart(el);
     const opcoes = this.obterOpcoes();
 
     opcoes['pieHole'] = 0.4;
-    chart.draw(this.obterDataTable(),opcoes);
+    chart.draw(this.obterDataTable(), opcoes);
   }
 
   /**
-   *Exibe Gráfico Bar Chart 
+   * Exibe o gráfico Bar Chart.
    *
-   *@return void
-   * */
-
+   * @return void
+   */
   exibirBarChart(): void {
-    const el = document.getElementById('bar_chart');
+  	const el = document.getElementById('bar_chart');
     const chart = new google.visualization.BarChart(el);
 
     chart.draw(this.obterDataTable(), this.obterOpcoes());
   }
 
   /**
-   *Exibe o gráfico Line Chart
-   *@return void
-   * */
-
+   * Exibe o gráfico Line Chart.
+   *
+   * @return void
+   */
   exibirLineChart(): void {
-    const el = document.getElementById('line_chart');
+  	const el = document.getElementById('line_chart');
     const chart = new google.visualization.LineChart(el);
-
+    
     chart.draw(this.obterDataTable(), this.obterOpcoes());
   }
 
+  /**
+   * Exibe o gráfico Column Chart.
+   *
+   * @return void
+   */
+  exibirColumnChart(): void {
+  	const el = document.getElementById('column_chart');
+    const chart = new google.visualization.ColumnChart(el);
+    
+    chart.draw(this.obterDataTable(), this.obterOpcoes());
+  }
 
-  /**Cria e retorna o objeto DataTable da API de gráficos, responsável por definir os dados gráficos*/
+  /**
+   * Cria e retorna o objeto DataTable da API de gráficos,
+   * responsável por definir os dados do gráfico.
+   *
+   * @return any
+   */
   obterDataTable(): any {
-    const data = new google.visualization.Datatable();
+  	const data = new google.visualization.DataTable();
 
     data.addColumn('string', 'Mês');
     data.addColumn('number', 'Quantidade');
@@ -132,35 +146,18 @@ export class DashboardComponent implements OnInit {
     return data;
   }
 
-
   /**
-   *
-   *
-   * Exibe o gráfico Column Chart
-   *
-   * @return void
-   *
-   * 
-
-  */
-
-  exibirColumnChart(): void {
-    const el = document.getElementById('column_chart');
-    const chart = new google.visualization.ColumnChart(el);
-
-    chart.draw(this.obterDataTable(), this.obterOpcoes());
-  }
-
-  /**Retorna as opções do gráfico, que incluem o título e tamanho do gráfico
+   * Retorna as opções do gráfico, que incluem o título
+   * e tamanho do gráfico.
    *
    * @return any
-   **/
-
-  obterOpcoes(): any{
-    return {
-      'title': 'Quantidade de cadastro primeiro semestre',
-      'width': 400,
-      'height': 300
+   */
+  obterOpcoes(): any {
+  	return {
+    	'title': 'Quantidade de cadastros primeiro semestre',
+        'width': 400,
+        'height': 300
     };
-    }
   }
+
+}
